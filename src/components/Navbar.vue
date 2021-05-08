dfaa4596dd0abf8baa5105744f4808a122a0b02e
Frances D Pasulohan <franpas12123@gmail.com>
2021-05-08 15:25:24 +0800

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-navbar variant="dark" type="dark" >
+        <b-container>
+            <b-navbar-brand href="/">
+                <img src="https://placekitten.com/g/30/30" alt="Kitten"> {{title}}
+            </b-navbar-brand>
+        </b-container>
+    </b-navbar>
+</template>
+<script>
+export default {
+    name: 'Navbar',
+    props: {title: String},
+}
+</script>
+<style>
+</style>
